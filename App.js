0d67e21270805b20d407e77fb95837579931695d
Santanu <sddutta90@gmail.com>
2026-04-19 14:47:20 +0530

    Add button and style updates to App component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.text}>Hello, World!!</Text>
+      <Button accessibilityLabel='T' color="#35546e" title="Click me!" onPress={() => alert('Button clicked!')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    margin: 20,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
   },
 });
